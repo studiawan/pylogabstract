0a0a9c678b4f10267ec7ec8a77d588d68c2cbcc8
Hudan Studiawan <studiawan@gmail.com>
2018-09-08 09:16:23 +0800

    Add master label and use OrderedDict

--- pylogabstract/parser/parser.py
+++ pylogabstract/parser/parser.py
@@ -1,4 +1,4 @@
-from collections import defaultdict
+from collections import OrderedDict
 from pylogabstract.parser.model.ner_model import NERModel
 from pylogabstract.parser.model.config import Config
 
@@ -7,37 +7,57 @@ class Parser(object):
     def __init__(self, log_file):
         self.log_file = log_file
         self.model = None
+        self.config = None
+        self.master_label = None
 
     def __load_pretrained_model(self):
         # create instance of config
-        config = Config()
+        self.config = Config()
 
         # load pretrained model
-        self.model = NERModel(config)
+        self.model = NERModel(self.config)
         self.model.build()
-        self.model.restore_session(config.dir_model)
+        self.model.restore_session(self.config.dir_model)
 
-    @staticmethod
-    def __get_per_entity(words_raw, ner_label):
+    def __load_label(self):
+        # load NER label and its corresponding human-readable field label
+        with open(self.config.label_file, 'r') as f:
+            label = f.readlines()
+
+        labels = {}
+        for line in label:
+            line_split = line.split()
+            ner_label, final_label = line_split[0], line_split[1]
+            labels[ner_label] = final_label
+
+        return labels
+
+    def __get_per_entity(self, words_raw, ner_label):
         # one entity can contain one or more words
-        entity = defaultdict(list)
+        entity = OrderedDict()
         for index, label in enumerate(ner_label):
             if '-' in label:
-                final_label = label.split('-')[1]
+                main_label = label.split('-')[1]
             else:
-                final_label = label
+                main_label = label
 
-            entity[final_label].append(words_raw[index])
+            if main_label not in entity.keys():
+                entity[main_label] = []
+
+            entity[main_label].append(words_raw[index])
 
         # one entity is now one sentence
-        for final_label, words in entity.items():
-            entity[final_label] = ' '.join(words)
+        final_entity = OrderedDict()
+        for main_label, words in entity.items():
+            final_label = self.master_label[main_label]
+            final_entity[final_label] = ' '.join(words)
 
-        return entity
+        return final_entity
 
     def parse_logs(self):
         # parse log files using pretrained model
         self.__load_pretrained_model()
+        self.master_label = self.__load_label()
         with open(self.log_file) as f:
             for line in f:
                 words_raw = line.strip().split(' ')
