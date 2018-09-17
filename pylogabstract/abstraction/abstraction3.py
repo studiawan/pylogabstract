3132fd1fd8bbd64bbdafb4376e194c79611fe388
Hudan Studiawan <studiawan@gmail.com>
2018-09-18 05:28:29 +0800

    Latest best config: use clustering in each group of message length

--- pylogabstract/abstraction/abstraction3.py
+++ pylogabstract/abstraction/abstraction3.py
@@ -54,10 +54,40 @@ class LogAbstraction(object):
 
         return clusters, modularity
 
+    def __get_valid_graph(self, graph):
+        # remove nodes without edges
+        isolated_nodes = nx.isolates(graph)
+        isolated = []
+        for node in isolated_nodes:
+            isolated.append(node)
+            self.clusters[self.cluster_id].append(node)
+            self.cluster_id += 1
+        graph.remove_nodes_from(isolated)
+
+        if graph.edges():
+            # if only one edge exist, no clustering
+            if len(graph.edges()) == 1:
+                self.clusters[self.cluster_id].extend(graph.nodes())
+                self.cluster_id += 1
+                return None
+            else:
+                return graph
+        else:
+            return None
+
+    @staticmethod
+    def __get_edge_weight_sum(graph):
+        weight_sum = 0
+        for edges in graph.edges(data='weight'):
+            weight_sum += edges[2]
+
+        return weight_sum
+
     def __get_graph_cluster(self, graph):
         # the initial max modularity is the modularity of the initial graph before clustering
         # the initial cluster configuration is the graph's connected component
-        best_cluster, max_modularity = self.__get_initial_modularity(graph)
+        # best_cluster, max_modularity = self.__get_initial_modularity(graph)
+        best_cluster, max_modularity = [], -1.
 
         # clustering using Girvan-Newman method
         clusters = community.girvan_newman(graph, most_valuable_edge=lightest)
@@ -70,7 +100,7 @@ class LogAbstraction(object):
             if max_modularity < modularity:
                 max_modularity = modularity
                 best_cluster = cluster
-                best_cluster = self.__convert_to_clusterid_nodeid(best_cluster)
+        best_cluster = self.__convert_to_clusterid_nodeid(best_cluster)
 
         return best_cluster
 
@@ -82,6 +112,7 @@ class LogAbstraction(object):
         self.event_attributes = preprocess.event_attributes
 
         for message_length, group in self.message_length_group.items():
+            print(message_length, group)
             # no graph needed
             group_length = len(group)
             if group_length == 1:
@@ -94,15 +125,14 @@ class LogAbstraction(object):
                 graph_model = CreateGraph(unique_events, self.event_attributes, group)
                 graph = graph_model.create_graph()
 
-                # clustering if edges exist
-                if graph.edges():
+                # only process connected components with edges > 1
+                graph = self.__get_valid_graph(graph)
+
+                # clustering with valid graph only
+                if graph is not None:
                     clusters = self.__get_graph_cluster(graph)
                     for index, nodes in clusters.items():
-                        self.clusters[self.cluster_id] = nodes
-                        self.cluster_id += 1
-                else:
-                    for node_id in group:
-                        self.clusters[self.cluster_id].append(node_id)
+                        self.clusters[self.cluster_id].extend(nodes)
                         self.cluster_id += 1
 
     def get_abstraction(self):
@@ -116,11 +146,12 @@ def lightest(g):
 
 
 if __name__ == '__main__':
-    logfile = '/home/hudan/Git/prlogparser/datasets/casper-rw/debug'
+    logfile = '/home/hudan/Git/prlogparser/datasets/casper-rw/kern.log'
     log_abstraction = LogAbstraction(logfile)
     results = log_abstraction.get_abstraction()
     event_attr = log_abstraction.event_attributes
 
+    print('\nCLUSTERING RESULT')
     for i, result in results.items():
         for r in result:
             print(r, event_attr[r]['message'])
