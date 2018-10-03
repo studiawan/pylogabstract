12d2850ba122061cbf27f4d940c683f387e8fff3
Hudan Studiawan <studiawan@gmail.com>
2018-10-03 09:11:19 +0800

    Add output package

--- pylogabstract/output/write_text.py
+++ pylogabstract/output/write_text.py
@@ -1,8 +1,12 @@
 
-class WriteAbstraction(object):
+class WriteText(object):
     def __init__(self):
         pass
 
     @staticmethod
     def abstraction_only(output_file, abstractions):
+        pass
+
+    @staticmethod
+    def abstraction_with_logs(output_file, abstractions, parsed_logs):
         pass
