feb1e1a702b022c6471e22bfdfcfa39d828dd678
Hudan Studiawan <studiawan@gmail.com>
2018-10-08 12:52:15 +0800

    Add utility for abstraction

---
+++ pylogabstract/abstraction/abstraction_utility.py
@@ -0,0 +1,40 @@
+import json
+
+
+class AbstractionUtility(object):
+    @staticmethod
+    def read_json(json_file):
+        # read json data
+        with open(json_file, 'r') as f:
+            data = json.load(f)
+
+        # change json key string to int
+        converted_data = {}
+        for key, value in data.iteritems():
+            converted_data[int(key)] = value
+
+        return converted_data
+
+    @staticmethod
+    def get_abstractionid_from_groundtruth(abstraction_withid_file, abstractions):
+        # read ground truth
+        abstraction_withid = AbstractionUtility.read_json(abstraction_withid_file)
+        groundtruth_length = len(abstraction_withid.keys())
+
+        abstractions_edited_id = {}
+        for abstraction_id, abstraction in abstractions.items():
+            # if abstraction exist in ground truth, get id from dictionary key
+            new_id = -1
+            for groundtruth_id, groundtruth_abstraction in abstraction_withid.items():
+                if abstraction['abstraction'] in groundtruth_abstraction:
+                    new_id = groundtruth_id
+
+            # if not exist, new id is dictionary length + 1
+            if new_id == -1:
+                new_id = groundtruth_length
+                groundtruth_length += 1
+
+            # set new abstraction id based on ground truth id
+            abstractions_edited_id[new_id] = abstraction
+
+        return abstractions_edited_id
