6f5eb0660c8628c1d832a8d22c4d0f4446aebe4f
Hudan Studiawan <studiawan@gmail.com>
2018-11-17 09:06:36 +0800

    Edit method when processing input file

--- pylogabstract/misc/misc_utility.py
+++ pylogabstract/misc/misc_utility.py
@@ -2,31 +2,28 @@ from pylogabstract.parser.parser import Parser
 
 
 class MiscUtility(object):
-    def __init__(self, log_file, output_file):
-        self.log_file = log_file
-        self.output_file = output_file
-        self.parsed_logs = {}
+    def __init__(self):
+        self.parser = Parser()
 
-    def __parser(self):
+    def __parser(self, log_file):
         # parse log files, only message field given to the method
-        parser = Parser()
-        parsed_results, raw_results = parser.parse_logs(self.log_file)
-        self.parsed_logs = parsed_results
+        parsed_results, raw_results = self.parser.parse_logs(log_file)
+        return parsed_results
 
-    def write_parsed_message(self):
+    def write_parsed_message(self, log_file, output_file):
         # write message fields from all log entries to file
         # this file will be read by a particular method
-        self.__parser()
-        f = open(self.output_file, 'w')
-        for entity, value in self.parsed_logs.items():
+        parsed_logs = self.__parser(log_file)
+        f = open(output_file, 'w')
+        for entity, value in parsed_logs.items():
             f.write(value['message'] + '\n')
         f.close()
 
-        return self.parsed_logs
+        return parsed_logs
 
 
 if __name__ == '__main__':
     logfile = '/home/hudan/Git/pylogabstract/datasets/casper-rw/logs/daemon.log'
     outputfile = 'test.log'
-    utility = MiscUtility(logfile, outputfile)
-    utility.write_parsed_message()
+    utility = MiscUtility()
+    utility.write_parsed_message(logfile, outputfile)
