a90620d675c96701f6471b5ca6f61df61355444e
Hudan Studiawan <studiawan@gmail.com>
2018-11-22 11:56:35 +0800

    Add raw_logs as return value

--- pylogabstract/misc/misc_utility.py
+++ pylogabstract/misc/misc_utility.py
@@ -9,12 +9,12 @@ class MiscUtility(object):
     def __parser(self, log_file):
         # parse log files, only message field given to the method
         parsed_results, raw_results = self.parser.parse_logs(log_file)
-        return parsed_results
+        return parsed_results, raw_results
 
     def write_parsed_message(self, log_file, output_file):
         # write message fields from all log entries to file
         # this file will be read by a particular method
-        parsed_logs = self.__parser(log_file)
+        parsed_logs, raw_logs = self.__parser(log_file)
         f = open(output_file, 'w')
         for log_id, value in parsed_logs.items():
             if value['message'] not in ['', '\n', '\r\n']:
@@ -23,7 +23,7 @@ class MiscUtility(object):
                 f.write('pylogabstract: no message found' + '\n')
         f.close()
 
-        return parsed_logs
+        return parsed_logs, raw_logs
 
     @staticmethod
     def get_cluster_number(groundtruth_file):
